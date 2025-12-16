4a6587db019f4ed190f4ae2eed9f640be4f953f3
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-16 17:41:06 +0800

    feat: add validation for commit hash and version format in HealthCheck model

--- docuisine/schemas/meta.py
+++ docuisine/schemas/meta.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class Status(str, Enum):
@@ -10,3 +10,20 @@ class Status(str, Enum):
 
 class HealthCheck(BaseModel):
     status: Status = Field(..., example=Status.HEALTHY)
+    commit_hash: str = Field(..., example="a1b2c3d")
+    version: str = Field(..., example="1.0.0")
+
+    @field_validator("commit_hash", mode="before")
+    def check_commit(cls, hash: str) -> str:
+        if len(hash) != 7:
+            raise ValueError("Commit hash must be exactly 7 characters long.")
+        return hash
+
+    @field_validator("version", mode="before")
+    def check_version(cls, version: str) -> str:
+        if version.count(".") != 2:
+            raise ValueError("Version must be in the format 'X.Y.Z'.")
+
+        if not all(part.isdigit() for part in version.split(".")):
+            raise ValueError("Version parts must be numeric.")
+        return version
