f08af648cd654ad81104ba2a41471dc583b4a9eb
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-16 22:28:15 +0800

    test: add tests for validation functions

---
+++ tests/schemas/test_common.py
@@ -0,0 +1,27 @@
+from pytest import raises
+
+from docuisine.schemas.common import _has_only_digits, _has_two_dots
+
+
+def test_has_two_dots_valid():
+    assert _has_two_dots("1.0.0") == "1.0.0"
+    assert _has_two_dots("10.20.30") == "10.20.30"
+
+
+def test_has_two_dots_invalid():
+    with raises(ValueError, match="Version must have two dots"):
+        _has_two_dots("1.0")
+    with raises(ValueError, match="Version must have two dots"):
+        _has_two_dots("1.0.1.1")
+
+
+def test_has_only_digits_valid():
+    assert _has_only_digits("1.0.0") == "1.0.0"
+    assert _has_only_digits("10.20.30") == "10.20.30"
+
+
+def test_has_only_digits_invalid():
+    with raises(ValueError, match="Version parts must be numeric"):
+        _has_only_digits("1.a.0")
+    with raises(ValueError, match="Version parts must be numeric"):
+        _has_only_digits("1.0.b")
