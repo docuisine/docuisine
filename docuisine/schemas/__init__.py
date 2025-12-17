7c95cafd0c49bd942c711c87da7f2360526988cb
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-17 22:40:47 +0800

    refactor: remove redundant response classes

--- docuisine/schemas/__init__.py
+++ docuisine/schemas/__init__.py
@@ -1,11 +1,7 @@
-from .enums import ErrorCode, Role, Status
+from .enums import Role, Status
 from .health import HealthCheck
-from .response import Error, Response
 
 __all__ = [
-    "Response",
-    "Error",
-    "ErrorCode",
     "HealthCheck",
     "Status",
     "Role",
--- docuisine/schemas/enums.py
+++ docuisine/schemas/enums.py
@@ -1,12 +1,6 @@
 from enum import Enum
 
 
-class ErrorCode(str, Enum):
-    NOT_FOUND = "not_found"
-    VALIDATION_ERROR = "validation_error"
-    SERVER_ERROR = "server_error"
-
-
 class Status(str, Enum):
     HEALTHY = "healthy"
     UNHEALTHY = "unhealthy"
