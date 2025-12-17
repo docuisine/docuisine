6ad5295426046e4565caeb84998b0d96b31fde51
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-17 22:25:33 +0800

    chore: remove legacy code

--- docuisine/main.py
+++ docuisine/main.py
@@ -15,11 +15,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.get("/health", response_model=HealthCheck)
 def health_check():
     return HealthCheck(
