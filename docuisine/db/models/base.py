540ddb1635d04cabd50cc53fc7108ecefb8d80a2
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-18 12:06:18 +0800

    refactor: make a base entity for common attributes

--- docuisine/db/models/base.py
+++ docuisine/db/models/base.py
@@ -1,7 +1,28 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime, timezone
+from typing import Optional
+
+from sqlalchemy import TIMESTAMP
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     def as_dict(self) -> dict:
         """Convert the User instance to a dictionary representation."""
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Entity:
+    """
+    Base entity model with common attributes.
+
+    Attributes:
+        id (int): Primary key identifier for the entity.
+        preview_img (Optional[str]): URL or path to the preview image.
+        img (Optional[str]): URL or path to the main image.
+        created_at (datetime): Timestamp when the entity was created.
+    """
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    preview_img: Mapped[Optional[str]]
+    img: Mapped[Optional[str]]
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.now(timezone.utc))
--- docuisine/db/models/user.py
+++ docuisine/db/models/user.py
@@ -1,32 +1,23 @@
-from datetime import datetime, timezone
-
-from sqlalchemy import DateTime, Integer, String
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from docuisine.schemas import Role
 
-from .base import Base
+from .base import Base, Entity
 
 
-class User(Base):
+class User(Base, Entity):
     """
     User model representing a user in the system.
 
     Attributes:
-        id (int): Primary key identifier for the user.
         email (str): Unique email address of the user.
         password (str): Hashed password of the user.
         role (str): Role of the user in the system (e.g., admin, user).
-        created_at (datetime): Timestamp when the user was created.
     """
 
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str]
     role: Mapped[str] = mapped_column(String, nullable=False, default=Role.USER.value)
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, default=datetime.now(timezone.utc)
-    )
-
