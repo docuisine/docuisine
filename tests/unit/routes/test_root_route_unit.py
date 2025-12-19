41bcae82f0259aa17b31eb491962b448fc2dfb2c
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-19 08:39:30 +0800

    test: move conftest.py contents to the only module that needs it

--- tests/unit/routes/test_root_route_unit.py
+++ tests/unit/routes/test_root_route_unit.py
@@ -1,4 +1,21 @@
+from unittest.mock import MagicMock
+
+from fastapi.testclient import TestClient
+
+from docuisine.dependencies.services import get_user_service
+from docuisine.main import app
+
+
 def test_read_root(client):
+    ## Setup
+    def mock_user_service():
+        mock = MagicMock()
+        return mock
+
+    app.dependency_overrides[get_user_service] = lambda: mock_user_service
+    client = TestClient(app)
+
+    ## Test
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Hello, from Docuisine!"
