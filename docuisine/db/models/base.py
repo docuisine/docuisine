e46fc4c899d7b49b7aada36888dffc75d727f49b
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-20 13:45:04 +0800

    refactor: implement Entity and Default tables

--- docuisine/db/models/base.py
+++ docuisine/db/models/base.py
@@ -9,18 +9,33 @@ class Base(DeclarativeBase):
     pass
 
 
-class Entity:
+class Default:
     """
-    Base entity model with common attributes.
+    Default model with common attributes.
 
     Attributes:
-        id (int): Primary key identifier for the entity.
+        created_at (datetime): Timestamp when the record was created.
+        updated_at (datetime): Timestamp when the record was last updated.
+    """
+
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.now(timezone.utc))
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP,
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc),
+    )
+
+
+class Entity(Default):
+    """
+    Base entity model with common attributes.
+    These entities should reference real-world objects.
+
+    Attributes:
         preview_img (Optional[str]): URL or path to the preview image.
         img (Optional[str]): URL or path to the main image.
         created_at (datetime): Timestamp when the entity was created.
     """
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     preview_img: Mapped[Optional[str]]
     img: Mapped[Optional[str]]
-    created_at: Mapped[datetime] = mapped_column(TIMESTAMP, default=datetime.now(timezone.utc))
--- docuisine/db/models/user.py
+++ docuisine/db/models/user.py
@@ -18,6 +18,7 @@ class User(Base, Entity):
 
     __tablename__ = "users"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=True)
     password: Mapped[str]
