30e089a25efca6dc80a95a43ada1be1878e33468
Chris Irag <chrisandrei.irag@1.ustp.edu.ph>
2025-12-23 16:08:47 +0800

    feat: add AuthorizedUser dependency

--- docuisine/dependencies/auth.py
+++ docuisine/dependencies/auth.py
@@ -1,10 +1,30 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-OAuth2_Scheme = OAuth2PasswordBearer(tokenUrl="token")
+from docuisine.db.models import User
+from docuisine.utils import errors
+
+from .services import User_Service
+
+OAuth2_Scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
 AuthToken = Annotated[str, Depends(OAuth2_Scheme)]
 AuthForm = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
+async def get_current_user_token(token: AuthToken, user_service: User_Service):
+    try:
+        user = user_service.authorize_user(token=token)
+    except errors.InvalidCredentialsError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Could not validate credentials: {e}",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+AuthorizedUser = Annotated[User, Depends(get_current_user_token)]
